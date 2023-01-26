58c77f6a78554f0f101e01c2d79100f6a8d4fccc
Jules de Smit <julesdesmit@gmail.com>
2023-01-26 23:48:08 +0400

    DRY

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! This library implements the SuperNova prover-verifier algorithm, and is generic
+//! over any kind of arithmetization, as long as it implements the [`Arithmetization`]
+//! and [`FoldedArithmetization`] traits.
+
+#![allow(non_snake_case)]
+
 mod arithmetization;
 use arithmetization::*;
 mod errors;
@@ -34,119 +40,101 @@ impl<A: Arithmetization, F: FoldedArithmetization<A>, const L: usize> Proof<A, F
         self.latest = next;
         self.pc = pc;
         self.i += 1;
-        let mut poseidon: Poseidon<Fr, 5, 4> = Poseidon::new(8, 5);
-        poseidon.update(
-            [self
-                .folded
-                .iter()
-                .fold(Fr::zero(), |acc, pair| acc + pair.params())]
-            .into_iter()
-            .chain([Fr::from(self.i as u64)])
-            .chain([Fr::from(self.pc as u64)])
-            .chain(self.latest.z0())
-            .chain(self.latest.inputs())
-            .chain(
-                [self
-                    .folded
-                    .iter()
-                    .fold(Fr::zero(), |acc, pair| acc + pair.digest())]
-                .into_iter(),
-            )
-            .collect::<Vec<Fr>>()
-            .as_slice(),
-        );
-        let x = poseidon.squeeze();
-        self.latest.push_hash(x);
+        self.latest.push_hash(hash_public_io(
+            self.folded,
+            self.i,
+            self.pc,
+            self.latest.z0(),
+            self.latest.inputs(),
+        ));
     }
 }
 
-/// Anb instantiation of a SuperNova verifier for a specific set of
-/// circuit parameters.
-///
-/// NOTE: may be redundant, the proof already contains the parameters.
-pub struct Verifier<const L: usize> {
-    params: [Fr; L],
+/// Verify a SuperNova proof.
+pub fn verify<A: Arithmetization, F: FoldedArithmetization<A>, const L: usize>(
+    proof: Proof<A, F, L>,
+) -> Result<(), VerificationError> {
+    // If this is only the first iteration, we can skip the other checks,
+    // as no computation has been folded.
+    if proof.i == 0 {
+        if proof.folded.iter().any(|pair| !pair.is_zero()) {
+            return Err(VerificationError::ExpectedBaseCase);
+        }
+
+        if !proof.latest.is_zero() {
+            return Err(VerificationError::ExpectedBaseCase);
+        }
+
+        return Ok(());
+    }
+
+    // Check that the public IO of the latest instance includes
+    // the correct hash.
+    let hash = hash_public_io(
+        proof.folded,
+        proof.i,
+        proof.pc,
+        proof.latest.z0(),
+        proof.latest.inputs(),
+    );
+    if proof.latest.public_inputs()[0] != hash {
+        return Err(VerificationError::HashMismatch(
+            hash,
+            proof.latest.public_inputs()[0],
+        ));
+    }
+
+    // Ensure PC is within range.
+    if proof.pc > proof.folded.len() {
+        return Err(VerificationError::PCOutOfRange(
+            proof.pc,
+            proof.folded.len(),
+        ));
+    }
+
+    // Ensure the latest instance has no crossterms.
+    if proof.latest.has_crossterms() {
+        return Err(VerificationError::UnexpectedCrossterms);
+    }
+
+    // Ensure all folded instance/witness pairs are satisfied.
+    if proof.folded.iter().any(|pair| !pair.is_satisfied()) {
+        return Err(VerificationError::UnsatisfiedCircuit);
+    }
+
+    // Ensure the latest instance/witness pair is satisfied.
+    if !proof.latest.is_satisfied() {
+        return Err(VerificationError::UnsatisfiedCircuit);
+    }
+
+    Ok(())
 }
 
-impl<const L: usize> Verifier<L> {
-    /// Instantiate a new SuperNova verifier with the given verifier keys.
-    pub fn new(params: [Fr; L]) -> Self {
-        Self { params }
-    }
-
-    /// Verify a SuperNova proof.
-    pub fn verify<A: Arithmetization, F: FoldedArithmetization<A>, const PL: usize>(
-        &self,
-        proof: Proof<A, F, PL>,
-    ) -> Result<(), VerificationError> {
-        // If this is only the first iteration, we can skip the other checks,
-        // as no computation has been folded.
-        if proof.i == 0 {
-            if proof.folded.iter().any(|pair| !pair.is_zero()) {
-                return Err(VerificationError::ExpectedBaseCase);
-            }
-
-            if !proof.latest.is_zero() {
-                return Err(VerificationError::ExpectedBaseCase);
-            }
-
-            return Ok(());
-        }
-
-        // Check that the public IO of the latest instance includes
-        // the correct hash.
-        let mut poseidon: Poseidon<Fr, 5, 4> = Poseidon::new(8, 5);
-        poseidon.update(
-            [self
-                .params
+fn hash_public_io<A: Arithmetization, F: FoldedArithmetization<A>, const L: usize>(
+    folded: [F; L],
+    i: usize,
+    pc: usize,
+    z0: Vec<Fr>,
+    inputs: Vec<Fr>,
+) -> Fr {
+    let mut poseidon: Poseidon<Fr, 5, 4> = Poseidon::new(8, 5);
+    poseidon.update(
+        [folded
+            .iter()
+            .fold(Fr::zero(), |acc, pair| acc + pair.params())]
+        .into_iter()
+        .chain([Fr::from(i as u64)])
+        .chain([Fr::from(pc as u64)])
+        .chain(z0)
+        .chain(inputs)
+        .chain(
+            [folded
                 .iter()
-                .fold(Fr::zero(), |acc, params| acc + params)]
-            .into_iter()
-            .chain([Fr::from(proof.i as u64)])
-            .chain([Fr::from(proof.pc as u64)])
-            .chain(proof.latest.z0())
-            .chain(proof.latest.inputs())
-            .chain(
-                [proof
-                    .folded
-                    .iter()
-                    .fold(Fr::zero(), |acc, pair| acc + pair.digest())]
-                .into_iter(),
-            )
-            .collect::<Vec<Fr>>()
-            .as_slice(),
-        );
-        let hash = poseidon.squeeze();
-        if proof.latest.public_inputs()[0] != hash {
-            return Err(VerificationError::HashMismatch(
-                hash,
-                proof.latest.public_inputs()[0],
-            ));
-        }
-
-        // Ensure PC is within range.
-        if proof.pc > proof.folded.len() {
-            return Err(VerificationError::PCOutOfRange(
-                proof.pc,
-                proof.folded.len(),
-            ));
-        }
-
-        // Ensure the latest instance has no crossterms.
-        if proof.latest.has_crossterms() {
-            return Err(VerificationError::UnexpectedCrossterms);
-        }
-
-        // Ensure all folded instance/witness pairs are satisfied.
-        if proof.folded.iter().any(|pair| !pair.is_satisfied()) {
-            return Err(VerificationError::UnsatisfiedCircuit);
-        }
-
-        // Ensure the latest instance/witness pair is satisfied.
-        if !proof.latest.is_satisfied() {
-            return Err(VerificationError::UnsatisfiedCircuit);
-        }
-
-        Ok(())
-    }
+                .fold(Fr::zero(), |acc, pair| acc + pair.digest())]
+            .into_iter(),
+        )
+        .collect::<Vec<Fr>>()
+        .as_slice(),
+    );
+    poseidon.squeeze()
 }
